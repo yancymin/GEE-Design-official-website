53d9b0c310ae446047fb148829e4fde4b45dc0a8
yancymin <senseslofter@gmail.com>
2018-09-25 17:12:02 +0800

    build

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import VueContentPlaceholders from 'vue-content-placeholders'
 
-// Vue.use(VueContentPlaceholders)
 Vue.config.productionTip = false
 
 new Vue({
